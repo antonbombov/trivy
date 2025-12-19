e55f5e8f88f3ac456f80d88d765ccb48cc255aa7
antonbombov <anton.996zgr@gmail.com>
2025-12-19 10:28:09 +0700

    Add files via upload
    modifed trivy scan path (now not absolute. it's start from Scan folder in trivy.exe place)

--- scan-all.ps1
+++ scan-all.ps1
@@ -1,7 +1,11 @@
-Get-ChildItem -Path .\Scan\*.tar | ForEach-Object {
-    Write-Host "Scanning: $($_.Name)" -ForegroundColor Cyan
+# scan-all.ps1 powershell -ExecutionPolicy Bypass -File .\scan-all.ps1
+$scanFolder = ".\Scan"
+
+Get-ChildItem -Path $scanFolder\*.tar | ForEach-Object {
+    $relativePath = "$scanFolder\" + $_.Name
     $reportName = $_.BaseName + ".json"
-    .\trivy.exe image --input $_.FullName --format json | Set-Content -Encoding UTF8 $reportName
+    
+    Write-Host "Scanning: $($_.Name)" -ForegroundColor Cyan
+    .\trivy.exe image --input $relativePath --format json | Set-Content -Encoding UTF8 $reportName
     Write-Host "  -> $reportName" -ForegroundColor Green
 }
-Write-Host "`nAll done!" -ForegroundColor Cyan
